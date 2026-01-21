13e657168c68827d4d0aa7be88f34030815f884c
Kelsey Callington <kelsey.marie@live.co.uk>
2026-01-21 19:19:06 +0000

    task 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Task 1
 const clickMe = document.querySelector('#primary-action-btn');
 let clicked = false;
 
@@ -5,9 +6,28 @@ console.log(clickMe);
 
 clickMe.addEventListener('click', function (event) {
     if (clicked == false) {
-        clickMe.innerHTML = 'You clicked';
+        clickMe.textContent = 'You clicked';
         clickMe.classList.toggle('btn-primary');
         clickMe.classList.toggle('btn-success');
         clicked = true;
     }
 });
+
+// Task 2
+const incrementButton = document.querySelector('#increment-btn');
+const decrementButton = document.querySelector('#decrement-btn');
+let counterValue = document.querySelector('#counter-value');
+let counter = 0;
+
+incrementButton.addEventListener('click', function (event) {
+    counter += 1;
+    counterValue.textContent = counter;
+});
+
+decrementButton.addEventListener('click', function (event) {
+    if (counter > 0) {
+        counter -= 1;
+        counterValue.textContent = counter;
+    }
+});
+
